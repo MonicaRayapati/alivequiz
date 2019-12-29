23590c0fe430dc98fb834f110152069e159e8cb4
MonicaRayapati <MonicaRayapati@users.noreply.github.com>
2019-12-30 10:13:11 +1100

    merged the functions as much as possible

--- contracts/quiz.sol
+++ contracts/quiz.sol
@@ -2,7 +2,6 @@ pragma solidity ^0.5.11;
 
 contract quiz {
 
-    string public inputData;
     address public owner;
     uint public endTime;
 
@@ -14,31 +13,27 @@ contract quiz {
         {
             owner = msg.sender;
             statusNow = Status.unknown;
+            endTime = now + 90 seconds;
         }
     modifier isOwner(){
         require(msg.sender == owner, "Invalid Authentication");
         _;
     }
 
-    function getInput(string memory statusKey) public isOwner() {
-        inputData = statusKey;
-    }
-        function storageState() public isOwner(){
+    function setStatus(string memory inputData) public isOwner(){
         if(keccak256(abi.encodePacked(inputData)) == keccak256(abi.encodePacked("I am alive")) ){
             statusNow = Status.alive;
+            endTime = now + 90 seconds;
         }
-    }
-    function setTimer() public {
-        endTime = now + 60 seconds;
-    }
-    function isEnd() public isOwner(){
-        if(now >= endTime){
+        else if(now >= endTime)
+        {
             statusNow = Status.missing;
         }
     }
+
     function getStatus() public view returns(string memory){
-        if(Status.unknown == statusNow){ return "Unknown";}
-        if(Status.missing == statusNow){ return "Missing";}
-        if(Status.alive == statusNow){ return "Alive";}
+        if(now >= endTime){return "Missing";}
+        else if(Status.unknown == statusNow){return "Unknown";}
+        else if(Status.alive == statusNow){return "Alive";}
     }
 }
